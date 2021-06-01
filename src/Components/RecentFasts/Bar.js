d1e7220db03e454cceeacd8f72f0e9bfda0b853a
Prasanna Kumar <prasannasrk07@gmail.com>
2021-06-02 01:13:17 +0530

    bar height overflow fix

--- src/Components/RecentFasts/Bar.js
+++ src/Components/RecentFasts/Bar.js
@@ -6,8 +6,8 @@ const Bar = ({ fastData, longestFastDuration }) => {
   ) / 1000 / 60 / 60 )
   return (
     <div key={fastData._id} style={{ width: 90, minWidth: 90 }} className="h-full border-l border-r border-gray-200 flex flex-col justify-end items-center">
-      <div style={{ height: `${difference / longestFastDuration * 100}%` }} className="w-3 bg-green-400 rounded-full" />
-      <span className="text-xs font-light text-gray-500">{dayjs(fastData.date).format('MMM D')}</span>
+      <div style={{ height: `calc(${difference / longestFastDuration * 100}% - 16px)` }} className="w-3 bg-green-400 rounded-full" />
+      <span className="text-xs font-light text-gray-500">{dayjs(fastData.startedAt).format('MMM D')}</span>
     </div>
   );
 };
