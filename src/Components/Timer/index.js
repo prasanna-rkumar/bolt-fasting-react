d380d63d1eb02c34a0f34bad471ba977454e06b2
Prasanna Kumar <prasannasrk07@gmail.com>
2021-06-03 13:28:02 +0530

    remove unused context listener

--- src/Components/Timer/index.js
+++ src/Components/Timer/index.js
@@ -1,17 +1,11 @@
 import SelectGoal from "./SelectGoal"
 import Progress from "./Progress"
-/* import { useContext } from "react";
-import { CurrentFastingContext } from "../../Context/CurrentFastingContext"; */
 import ElapsedTime from "./ElapsedTime";
 import TimerButton from "./TimerButton";
 import Timings from "./Timings";
 import { CurrentFastingTimerProvider } from "../../Context/CurrentFastingTimerContext";
 
 const Timer = () => {
-  /* const { isLoading } = useContext(CurrentFastingContext);
-
-  console.log(isLoading) */
-
   return (
     <div className=" bg-primary rounded-3xl shadow-xl w-full flex flex-col items-center justify-between p-4" style={{
       maxWidth: 360,
